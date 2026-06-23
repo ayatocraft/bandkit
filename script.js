2ecbf70062f938d7a2f67af2c6d438b57b91fdd1
ayatocraft <ayatocraft1221@gmail.com>
2026-06-23 21:35:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -488,7 +488,7 @@ const LANG = {
         tuner: "Tuner",
         pitch: "Pitch Detection",
         piano: "Pianoshock 2",
-        score: "Music Score",
+        score: "Disabled",
         noise: "Noise Meter",
         settings: "Settings",
 
